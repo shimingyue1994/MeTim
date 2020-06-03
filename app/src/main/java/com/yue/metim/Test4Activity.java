d44677eb01f768f4cee0c74e46233421597b1f10
shimingyue <shimingyue@romens.cn>
2020-06-03 15:53:27 +0800

    图片等比缩放修改

--- app/src/main/java/com/yue/metim/Test4Activity.java
+++ app/src/main/java/com/yue/metim/Test4Activity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 
+import com.yue.libtim.utils.soft.SoftKeyBoardListener;
 import com.yue.metim.databinding.ActivityTest4Binding;
 
 public class Test4Activity extends AppCompatActivity {
@@ -16,6 +18,18 @@ public class Test4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_test4);
+
+        SoftKeyBoardListener.setListener(this, new SoftKeyBoardListener.OnSoftKeyBoardChangeListener() {
+            @Override
+            public void keyBoardShow(int height) {
+                Toast.makeText(Test4Activity.this, "键盘显示 高度" + height, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void keyBoardHide(int height) {
+                Toast.makeText(Test4Activity.this, "键盘隐藏 高度" + height, Toast.LENGTH_SHORT).show();
+            }
+        });
         mBinding.inputlayout.init(this);
 
     }
