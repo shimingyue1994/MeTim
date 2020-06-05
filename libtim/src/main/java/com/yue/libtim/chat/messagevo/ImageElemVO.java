5bafe9a2303b9617c0b7d3af02b7fdd44fc2616d
shimingyue <shimingyue@romens.cn>
2020-06-05 16:23:48 +0800

    添加录音监听

--- libtim/src/main/java/com/yue/libtim/chat/messagevo/ImageElemVO.java
+++ libtim/src/main/java/com/yue/libtim/chat/messagevo/ImageElemVO.java
@@ -11,6 +11,8 @@ import com.tencent.imsdk.v2.V2TIMMessage;
 public class ImageElemVO extends BaseMsgElem<String, V2TIMImageElem> {
 
     private int sendProgress = 0;//发送的进度
+    private int showWidth;//展示高度，在选择图片的时候就设置，这样可以防止更新和滑动到底部的方法不同步的问题
+    private int showHeight;
 
     /**
      * @param timMessage 消息
